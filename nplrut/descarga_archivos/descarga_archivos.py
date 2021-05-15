8d03a4e80bb9a6579fa8be6dfec0cb13e222cfa5
chosky <davidhenaoo98@gmail.com>
2021-05-15 16:41:54 -0500

    se arregla error de novato

--- nplrut/descarga_archivos/descarga_archivos.py
+++ nplrut/descarga_archivos/descarga_archivos.py
@@ -21,7 +21,7 @@ def descarga_archivos():
         "mensaje": ""
     }
     nombre_archivo_rut = secure_filename(archivo_rut.filename)
-    if nombre_archivo_rut == "":
+    if nombre_archivo_rut != "":
         mensaje_salida = validaciones_archivo(nombre_archivo_rut, mensaje_salida)
         if mensaje_salida["tipo"] == "Correcto":
             print(mensaje_salida["mensaje"])
